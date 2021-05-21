0cc05927b12c6763639daa1b2c5a64a88e85194f
SeanyCarol <seany@ice.ufjf.br>
2021-05-21 02:21:39 -0300

    Implements Collection Resource

---
+++ src/main/java/com/algaworks/algalog/domain/model/Client.java
@@ -0,0 +1,14 @@
+package com.algaworks.algalog.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Client {
+
+  private Long id;
+  private String name;
+  private String email;
+  private String telephone; 
+}
