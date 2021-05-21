10a735dcbc28e39a493d5d95174ded7f63ca9f9c
SeanyCarol <seany@ice.ufjf.br>
2021-05-21 19:17:25 -0300

    Using Spring Data JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,23 +1,22 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List; 
 
 import com.algaworks.algalog.domain.model.Client;
+import com.algaworks.algalog.domain.repository.ClientRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
 
-  @PersistenceContext
-  private EntityManager entityManager;
+  @Autowired
+  private ClientRepository clientRepository;
 
   @GetMapping("/clientes")
   public List<Client> listar() {
-    return entityManager.createQuery("from Client", Client.class).getResultList();
+    return clientRepository.findAll();
   }
 }
---
+++ src/main/java/com/algaworks/algalog/domain/repository/ClientRepository.java
@@ -0,0 +1,15 @@
+package com.algaworks.algalog.domain.repository;
+
+import java.util.List;
+
+import com.algaworks.algalog.domain.model.Client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+  
+  List<Client> findByName(String name);
+  List<Client> findByNameContaining(String name);
+}
